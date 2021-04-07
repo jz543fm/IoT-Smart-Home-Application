8415e4d460c704f3fd00bbd0e7b93000514c4bce
Your Name <juraj.zittnansky@student.tuke.sk>
2021-04-07 08:10:44 +0200

    Initial Commit

---
+++ backend/src/test/java/sk/tuke/fei/kpi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.tuke.fei.kpi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
